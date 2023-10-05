b4798379ee92912c29350676ab22588d60951500
Amah Nnachi Lofty <loftdot@gmail.com>
2023-10-05 21:11:57 +0100

    Multi-language script using an array of strings and a loop

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+// Multi-language script using an array of strings and a loop
+// Array of strings
+
+const languages = ['C', 'Python', 'JavaScript'];
+// Loop through the array and print the lines using one console.log
+for (const language of languages) {
+  console.log(`${language} is amazing`);
+}

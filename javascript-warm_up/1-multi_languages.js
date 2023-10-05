d31b5ce7f709b3aa6c6e25ee16f127942ff05574
Amah Nnachi Lofty <loftdot@gmail.com>
2023-10-05 20:44:11 +0100

    Using const to declare variable.

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,16 @@
+/*Using const to declare variable.
+Write a script that prints 3 lines:
+
+    The first line: “C is fun”
+    The second line: “Python is cool”
+    The third line: “JavaScript is amazing”
+*/
+
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
+
+// Printing the lines using console.log
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);

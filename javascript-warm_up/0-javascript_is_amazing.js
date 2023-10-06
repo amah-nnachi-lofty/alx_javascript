31e6a405bda428b68f4f6020544aca8ca8703c3f
Amah Nnachi Lofty <loftdot@gmail.com>
2023-10-06 06:19:16 +0100

    Creating a constant variable called myVar with the value JavaScript is amazing

---
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*the script should be run using the Node.js interpreter located at /usr/bin/node.
+With shebang line #!/usr/bin/node at the beginning, you can run the script using the following command in the terminal:
+    $ ./0-javascript_is_amazing.js
+Without the shebang line, you would need to run the script using the node command explicitly:
+    $ node s0-javascript_is_amazing.js
+*/
+
+// Creating a constant variable called myVar with the value "JavaScript is amazing"
+const myVar = 'JavaScript is amazing';
+
+// Printing the output using console.log
+console.log(myVar);

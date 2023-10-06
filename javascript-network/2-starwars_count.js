f547631183cab64b7489da811c8751237d4f440f
Amah Nnachi Lofty <loftdot@gmail.com>
2023-10-06 17:42:55 +0100

    Write a script that prints the number of movies where the character “Wedge Antilles” is present.

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,38 +1,31 @@
-#!/usr/bin/node
-
-// Import the required 'request' module.
-const request = require('request');
-
-/**
- * Function to count the number of movies where the character "Wedge Antilles" is present.
- * @param {string} apiUrl - The API URL of the Star Wars films endpoint.
+/* Prints the number of movies where the character "Wedge Antilles" is present
+ * Takes args, first of which is the URL to request (GET) using the module 'request'
+ * Wedge Antilles is character ID 18 - use the ID to filter the results of the API
  */
-function countMoviesWithWedgeAntilles(apiUrl) {
-  // Make an HTTP GET request to the API endpoint.
-  request.get(apiUrl, (error, response, body) => {
-    // Handle potential errors.
-    if (error) {
-      console.error('Error:', error);
-    } else {
-      // Parse the JSON response body.
-      const filmsData = JSON.parse(body);
-      // Filter films where "Wedge Antilles" (character ID 18) is present.
-      const moviesWithWedgeAntilles = filmsData.results.filter((film) =>
-        film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
-      );
-      // Print the number of movies with "Wedge Antilles".
-      console.log(moviesWithWedgeAntilles.length);
+
+const req = require("request"); // Import the 'request' module.
+// Get the movie ID
+const url = `${process.argv[2]}`; //Star Wars API endpoint
+
+// Make a GET request to the API
+req(url, (error, response, body) => {
+  if (error) {
+    return console.error(error);
+  }
+
+  const data = JSON.parse(body);
+  const films = data.results;
+  let count = 0;
+  const characterId = "18"; // Character ID for Wedge Antilles
+
+  for (const film in films) {
+    const filmchars = films[film].characters;
+    for (const charIndex in filmchars) {
+      if (filmchars[charIndex].includes(characterId)) {
+        count += 1;
+      }
     }
-  });
-}
+  }
 
-// Check if the API URL is provided as a command line argument.
-if (process.argv.length !== 3) {
-  // If not, print an error message and exit the script.
-  console.error('Usage: node 2-starwars_count.js <API_URL>');
-  process.exit(1);
-}
-
-// Get the API URL from the command line arguments and call the countMoviesWithWedgeAntilles function.
-const apiUrl = process.argv[2];
-countMoviesWithWedgeAntilles(apiUrl);
+  console.log(count);
+});

d91de02caa9257326f516434ac5591a6f0d49687
Amah Nnachi Lofty <loftdot@gmail.com>
2023-10-06 13:34:04 +0100

    Update this script by adding a new function incr that increments the integer value

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -2,20 +2,21 @@
 
 const myObject = {
     type: "object",
-    // Property: type with the value "object"
     value: 12,
-    // Property: value with the initial value 12
-  };
-  console.log(myObject);
-  
-  myObject.incr = function () {
+};
+
+console.log(myObject);
+
+myObject.incr = function () {
     this.value++;
-    // Method: incr, increments the 'value' property of the object by 1
-  };
-  myObject.incr();
-  // Method incr() is called, incrementing 'value' from 12 to 13 and so forth
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
+    /* Method: incr, increments the 'value' property of the object by 1*/
+};
+
+myObject.incr();
+console.log(myObject); // Output: { type: 'object', value: 13, incr: [Function] }
+
+myObject.incr();
+console.log(myObject); // Output: { type: 'object', value: 14, incr: [Function] }
+
+myObject.incr();
+console.log(myObject); // Output: { type: 'object', value: 15, incr: [Function] }

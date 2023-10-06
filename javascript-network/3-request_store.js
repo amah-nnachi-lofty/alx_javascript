8ff6380ceae8cd45f70d38e5848ea620d3fd3970
Amah Nnachi Lofty <loftdot@gmail.com>
2023-10-06 18:01:05 +0100

    Write a script that gets the contents of a webpage and stores it in a file

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,42 +1,28 @@
 #!/usr/bin/node
 
 // Import the required modules.
-const request = require('request');
-const fs = require('fs');
+const fs = require('fs'); // File system module.
+const request = require('request'); // HTTP request module.
 
-/**
- * Function to request a webpage and store its content in a file.
- * @param {string} url - The URL of the webpage to request.
- * @param {string} filePath - The file path to store the response body.
- */
-function requestAndStore(url, filePath) {
-  // Make an HTTP GET request to the specified URL.
-  request.get(url, (error, response, body) => {
-    // Handle potential errors during the request.
-    if (error) {
-      console.error('Error:', error);
-    } else {
-      // Write the response body to the specified file path.
-      fs.writeFile(filePath, body, 'utf-8', (err) => {
-        // Handle errors during file write operation.
-        if (err) {
-          console.error('Error writing to file:', err);
-        } else {
-          console.log('File successfully written:', filePath);
-        }
-      });
-    }
-  });
-}
-
-// Check if both URL and file path are provided as command line arguments.
-if (process.argv.length !== 4) {
-  // If not, print an error message and exit the script.
-  console.error('Usage: node 3-request_store.js <url> <filePath>');
-  process.exit(1);
-}
-
-// Get URL and file path from the command line arguments and call the requestAndStore function.
+// Get the URL and file path from command line arguments.
 const url = process.argv[2];
 const filePath = process.argv[3];
-requestAndStore(url, filePath);
+
+// Make a GET request to the specified URL.
+request(url, (error, response, body) => {
+  // Handle potential errors.
+  if (error) {
+    console.error(error);
+  } else {
+    // Write the response body to the specified file path in UTF-8 encoding.
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      // Handle write errors.
+      if (err) {
+        console.error(err);
+      } else {
+        // Print a success message if the file was written successfully.
+        console.log();
+      }
+    });
+  }
+});
